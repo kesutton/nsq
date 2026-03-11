f42af1979cfe601a9dd474eae9339b8b31a3a401
Kelly Sutton <kesutton@users.noreply.github.com>
2026-03-11 13:24:02 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,6 +5,7 @@ GOMAXPROCS=1 go test -timeout 90s ./...
 GOMAXPROCS=4 go test -timeout 90s -race ./...
 
 # no tests, but a build is something
+# and this is a new line!
 for dir in $(find apps bench -maxdepth 1 -type d) nsqadmin; do
     if grep -q '^package main$' $dir/*.go 2>/dev/null; then
         echo "building $dir"
